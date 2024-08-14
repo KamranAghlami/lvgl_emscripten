5615088f66c7a8b71e37e7dbfa0d1d5a860f6ec4
Kamran Aghlami <oops.legendary@gmail.com>
2024-08-14 14:25:29 +0300

    cleaned main

--- src/application/application.h
+++ src/application/application.h
@@ -2,19 +2,24 @@
 
 #include <cstdint>
 #include <memory>
+#include <lvgl.h>
 
-#define DEFINE_MAIN(application) \
-    int main()                   \
-    {                            \
-        static application app;  \
-        return 0;                \
+#include "event/handler.h"
+#include "event/window.h"
+
+#define DEFINE_MAIN(class) \
+    int main()             \
+    {                      \
+        static class app;  \
+                           \
+        return 0;          \
     }
 
-class application
+class application : public event::handler<application>
 {
 public:
     application();
-    virtual ~application() = default;
+    virtual ~application();
 
     application(const application &) = delete;
     application(application &&) = delete;
@@ -25,5 +30,17 @@ protected:
     virtual void update(float timestep) = 0;
 
 private:
+    bool on_size_change(const event::window::size_changed &event);
+
+    void set_bitmap(const int x1, const int y1, const int x2, const int y2, uint8_t *buffer);
+
     float m_previous_timestamp = 0;
+
+    double m_scaling = 1.0f;
+    int m_width = 0;
+    int m_height = 0;
+
+    void *mp_draw_buf_1 = nullptr;
+    void *mp_draw_buf_2 = nullptr;
+    lv_display_t *mp_display = nullptr;
 };
