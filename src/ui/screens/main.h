8a8ed030666c5b7cfa1072dfa5c6c78e580476eb
Kamran Aghlami <oops.legendary@gmail.com>
2024-09-10 13:30:35 +0300

    remote todo

--- src/ui/screens/main.h
+++ src/ui/screens/main.h
@@ -13,7 +13,7 @@ namespace ui
     namespace screens
     {
         // TODO:
-        // styles selectors animations fonts ...
+        // styles animations fonts ...
 
         class main : public lvgl::screen
         {
