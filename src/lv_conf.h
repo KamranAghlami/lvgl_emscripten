651c666844c3f582fd0b515136a45598bac86ef6
Kamran <oops.legendary@gmail.com>
2025-03-28 14:26:43 +0330

    remove fps cap

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -72,7 +72,7 @@
  *====================*/
 
 /*Default display refresh, input device read and animation step period.*/
-#define LV_DEF_REFR_PERIOD  16      /*[ms]*/
+#define LV_DEF_REFR_PERIOD  1      /*[ms]*/
 
 /*Default Dot Per Inch. Used to initialize default sizes such as widgets sized, style paddings.
  *(Not so important, you can adjust it to modify default sizes and spaces)*/
